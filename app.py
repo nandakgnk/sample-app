9c1f6c970762788f47761732a5007cf3fb459b3c
nandakgnk <41625826+nandakgnk@users.noreply.github.com>
2023-07-20 19:44:36 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,16 +1,10 @@
-import os
 from flask import Flask
 app = Flask(__name__)
 
-
 @app.route('/')
-def index():
-    return "Welcome!"
-
-@app.route('/how are you')
-def hello():
-    retrun 'I am good'
+def hello_geek():
+    return '<h1>Hello from Flask & Docker</h2>'
 
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port-8080)
+if __name__ == "__main__":
+    app.run(debug=True)
